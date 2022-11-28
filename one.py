0b9ffe0ea70c1cd637ba3a17c5b3e834ca811fc0
Adil jaseem <65609243+jaseem1999@users.noreply.github.com>
2022-11-28 13:46:17 +0530

    python CO1

---
+++ one.py
@@ -0,0 +1,5 @@
+tuple_num=(1,2,8,4.5)
+print(tuple_num)
+print("length of ",len(tuple_num))
+print("max of tuple",max(tuple_num))
+print("min of tuple",min(tuple_num))
---
+++ two.py
@@ -0,0 +1,5 @@
+list=["apple","mango","Orange"]
+print(type(list))
+print("lis t : ",list)
+list = tuple(list)
+print("it s tuple type ",type(list))
